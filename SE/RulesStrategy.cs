3dad1309284fbedacd76bbb2a60dd761524e235c
Łukasz <luki356@hotmail.com>
2019-06-01 12:45:54 +0200

    0.4

--- SE/RulesStrategy.cs
+++ SE/RulesStrategy.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class RulesStrategy
+namespace SE
 {
-	public RulesStrategy()
-	{
-
-	}
+    class RulesStrategy
+    {
+    }
 }
